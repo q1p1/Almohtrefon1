dbb726848244b737e95455580143d33983075189
q1p1 <meshari.algahbban@wakecap.com>
2025-09-17 19:54:01 +0300

    deploy build to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
+  base: "/Almohtrefon1/",
   plugins: [react()],
-})
+});
